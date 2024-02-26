87fd3c7f095174c855075807fd6791f9827bcfbf
Sir <brudloord@yandex.ru>
2024-02-27 02:58:39 +0300

    Создал окошки.

---
+++ app/src/main/java/com/example/friendlocation/AllChats.java
@@ -0,0 +1,14 @@
+package com.example.friendlocation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AllChats extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_chats);
+    }
+}
---
+++ app/src/main/java/com/example/friendlocation/ParticularChat.java
@@ -0,0 +1,14 @@
+package com.example.friendlocation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ParticularChat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_particular_chat);
+    }
+}

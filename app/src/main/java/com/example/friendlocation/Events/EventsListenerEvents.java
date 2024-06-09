e438969ae67c2e4a1a0a6a9e88c77a71ab858515
Brudlord <brudloord@yandex.ru>
2024-06-09 20:42:10 +0300

    Сделал базовое удаление ивентов

--- app/src/main/java/com/example/friendlocation/Events/EventsListenerEvents.java
+++ app/src/main/java/com/example/friendlocation/Events/EventsListenerEvents.java
@@ -15,7 +15,14 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.HashMap;
+import java.util.Objects;
+
 public class EventsListenerEvents {
+
+    public static HashMap<String, ValueEventListener> eventListeners = new HashMap<>();
+
+    public static HashMap<String, Boolean> eventRemoved = new HashMap<>();
 
 
     public static void makeEventsList(EventAdapterEvents eventsAdapter, CalendarView calendarView, Context context, Resources resources) {
@@ -28,6 +35,10 @@ public class EventsListenerEvents {
                     public void onDataChange(DataSnapshot dataSnapshot) {
                         try {
                             Event ev = dataSnapshot.getValue(Event.class);
+                            if (eventRemoved.getOrDefault(ev.uid, false)){
+                                return;
+                            }
+                            eventListeners.put(ev.uid, this);
                             eventsAdapter.addEvent(ev);
                         } catch (Exception e) {
                             Log.e("Making marker", e.getMessage());
@@ -50,6 +61,8 @@ public class EventsListenerEvents {
             @Override
             public void onChildRemoved(DataSnapshot dataSnapshot) {
                 String commentKey = dataSnapshot.getKey();
+                eventRemoved.put(commentKey, true);
+                getDatabase().getReference("events").child(uid).removeEventListener(Objects.requireNonNull(eventListeners.get(commentKey)));
             }
 
             @Override

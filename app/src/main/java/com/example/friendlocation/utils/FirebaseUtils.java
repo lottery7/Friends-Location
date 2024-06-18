dca635e118c698e7ad8c101279b630718dd45e4f
Brudlord <brudloord@yandex.ru>
2024-06-18 17:41:22 +0300

    Слил ветки воедино

--- app/src/main/java/com/example/friendlocation/utils/FirebaseUtils.java
+++ app/src/main/java/com/example/friendlocation/utils/FirebaseUtils.java
@@ -9,6 +9,7 @@ import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.List;
 
 
 public class FirebaseUtils {

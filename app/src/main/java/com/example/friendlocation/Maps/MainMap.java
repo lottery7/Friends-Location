9f4d207cba1e91e658a75c594eeda58eb99ba4db
Brudlord <brudloord@yandex.ru>
2024-06-24 20:05:54 +0300

    Fast fix

--- app/src/main/java/com/example/friendlocation/Maps/MainMap.java
+++ app/src/main/java/com/example/friendlocation/Maps/MainMap.java
@@ -98,7 +98,7 @@ public class MainMap extends BottomBar implements OnMapReadyCallback, LocListene
             Button selectPlaceBtn = findViewById(R.id.select_place_btn);
             selectPlaceBtn.setVisibility(View.VISIBLE);
 
-            LinearLayout BottomBar = findViewById(R.id.bottom_bar);
+            BottomNavigationView BottomBar = findViewById(R.id.bottom_bar);
             BottomBar.setVisibility(View.INVISIBLE);
         }
         //   /--- new features for select mode ---/
